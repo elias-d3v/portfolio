1785bbc84ee8aceec0ee617a1cbc79a42f660b59
Elias-DEV <156446201+elias-d3v@users.noreply.github.com>
2024-03-08 11:53:56 +0000

    Update

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,26 @@ function changeDarkMode() {
 }
 
 
+
+const title = document.getElementById('autotext');
+const text = "Développeur fullstack.";
+
+let index = 0
+
+const randomSpeed = (min,max) => {
+    return Math.floor(Math.random() * (max - min) + min) 
+}
+
+const play = () => {
+    title.innerHTML = text.slice(0, index)
+
+    index++;
+
+    if (index > text.length) {
+        index=0;
+    }
+    clearInterval(timer)
+    timer = setInterval(play, randomSpeed(50,300))
+};
+
+let timer = setInterval(play, 300)
